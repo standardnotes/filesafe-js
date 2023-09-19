f656644abd50f28a984a739af62036fcdbe68709
Mo <mo@standardnotes.com>
2023-09-19 09:48:37 -0500

    fix: transition to filesafe.standardnotes.com

--- src/lib/RelayManager.js
+++ src/lib/RelayManager.js
@@ -1,83 +1,110 @@
-import "standard-file-js/dist/regenerator.js";
+import "standard-file-js/dist/regenerator.js"
 import {
   StandardFile,
   SFAbstractCrypto,
   SFItemTransformer,
-  SFHttpManager
-} from 'standard-file-js';
+  SFHttpManager,
+} from "standard-file-js"
 
 export default class RelayManager {
-
   constructor() {
-    this.httpManger = new SFHttpManager();
-    this.httpManger.setJWTRequestHandler(() => {});
+    this.httpManger = new SFHttpManager()
+    this.httpManger.setJWTRequestHandler(() => {})
   }
 
   setCredentials(credentials) {
-    this.credentials = credentials;
+    this.credentials = credentials
+  }
+
+  getIntegrationRelayUrl(integration) {
+    const url = integration.content.relayUrl
+    return url.replace(
+      "filesafe.standardnotes.org",
+      "filesafe.standardnotes.com"
+    )
   }
 
   async uploadFile(name, item, integration) {
-    const url = `${integration.content.relayUrl}/integrations/save-item`;
+    const url = `${this.getIntegrationRelayUrl(
+      integration
+    )}/integrations/save-item`
     const params = {
       file: {
         name: name,
-        item: item // base64 string of file
+        item: item, // base64 string of file
       },
       source: integration.content.source,
-      authorization: integration.content.authorization
+      authorization: integration.content.authorization,
     }
 
     return new Promise((resolve, reject) => {
-      this.httpManger.postAbsolute(url, params, (response) => {
-        resolve(response.metadata);
-      }, (errorResponse) => {
-        let error = errorResponse.error;
-        if (!error) {
-          error = {
-            message: "File upload failed."
-          };
+      this.httpManger.postAbsolute(
+        url,
+        params,
+        (response) => {
+          resolve(response.metadata)
+        },
+        (errorResponse) => {
+          let error = errorResponse.error
+          if (!error) {
+            error = {
+              message: "File upload failed.",
+            }
+          }
+          console.log("Upload error response", error)
+          reject(error)
         }
-        console.log("Upload error response", error);
-        reject(error);
-      })
-    });
+      )
+    })
   }
 
   async downloadFile(fileDescriptor, integration) {
-    const url = `${integration.content.relayUrl}/integrations/download-item`;
+    const url = `${this.getIntegrationRelayUrl(
+      integration
+    )}/integrations/download-item`
     const params = {
       metadata: fileDescriptor.content.serverMetadata,
-      authorization: integration.content.authorization
+      authorization: integration.content.authorization,
     }
 
     return new Promise((resolve, reject) => {
-      this.httpManger.postAbsolute(url, params, (response) => {
-        resolve(response);
-      }, (errorResponse) => {
-        const error = errorResponse.error;
-        console.log("Download error response", errorResponse);
-        reject(error);
-      })
-    });
+      this.httpManger.postAbsolute(
+        url,
+        params,
+        (response) => {
+          resolve(response)
+        },
+        (errorResponse) => {
+          const error = errorResponse.error
+          console.log("Download error response", errorResponse)
+          reject(error)
+        }
+      )
+    })
   }
 
   async deleteFile(fileDescriptor, integration) {
-    const url = `${integration.content.relayUrl}/integrations/delete-item`;
+    const url = `${this.getIntegrationRelayUrl(
+      integration
+    )}/integrations/delete-item`
     const params = {
       metadata: fileDescriptor.content.serverMetadata,
-      authorization: integration.content.authorization
+      authorization: integration.content.authorization,
     }
 
     return new Promise((resolve, reject) => {
-      this.httpManger.postAbsolute(url, params, (response) => {
-        resolve(response);
-      }, (errorResponse) => {
-        const error = errorResponse.error;
-        console.log("Download error response", errorResponse);
-        reject(error);
-      })
-    });
+      this.httpManger.postAbsolute(
+        url,
+        params,
+        (response) => {
+          resolve(response)
+        },
+        (errorResponse) => {
+          const error = errorResponse.error
+          console.log("Download error response", errorResponse)
+          reject(error)
+        }
+      )
+    })
   }
-
 }
